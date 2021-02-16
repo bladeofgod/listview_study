37a44c9053194e97640ff03248afcc5f4d2fa55e
bladeofgod <450103187@qq.com>
2021-02-16 09:13:34 +0800

    共享纹理换个项目做

---
+++ android/app/src/main/java/com/example/flutter_list_view_study/share_texture/ShareTexture.java
@@ -0,0 +1,15 @@
+package com.example.flutter_list_view_study.share_texture;
+
+import io.flutter.view.TextureRegistry;
+
+/**
+ * @author LiJiaqi
+ * @date 2021/2/15
+ * Description:
+ */
+class ShareTexture implements TextureRegistry {
+    @Override
+    public SurfaceTextureEntry createSurfaceTexture() {
+        return null;
+    }
+}
---
+++ lib/texture/share_texture.dart
@@ -0,0 +1,25 @@
+
+
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ShareTexturePage extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    return ShareTexturePageState();
+  }
+
+}
+
+class ShareTexturePageState extends State<ShareTexturePage>{
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Container(),
+    );
+  }
+}
